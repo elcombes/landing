13284c9f798c623ff2762a592a21f8d242760cb9
Marce Arce <marcearce@users.noreply.github.com>
2026-02-11 17:17:06 -0300

    envio de laead

---
+++ lead-proxy.php
@@ -0,0 +1,18 @@
+<?php
+
+$data = file_get_contents("php://input");
+
+$ch = curl_init("https://hooks.zapier.com/hooks/catch/1997286/uea8m3w/");
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+curl_setopt($ch, CURLOPT_HTTPHEADER, [
+    "Content-Type: application/json"
+]);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+$response = curl_exec($ch);
+$httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+curl_close($ch);
+
+http_response_code($httpCode);
+echo $response;
